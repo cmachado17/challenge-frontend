f5c6c67327a1f5e6fb4b4283d739cb3fe2b7d627
cmachado17 <machado.cristian1217@gmail.com>
2021-05-09 23:41:29 -0300

    estilos, maquetacion, navegacion en menu lateral para responsive

---
+++ scripts/index.js
@@ -0,0 +1,11 @@
+// Abrir menu lateral
+
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
+
+// Cerrar menu
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
